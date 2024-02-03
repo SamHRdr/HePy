440f94ab3770afa2344633712abf8d3740e8948c
SamHRdr <45273109+SamHRdr@users.noreply.github.com>
2024-02-03 23:55:30 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="HePy",
+    version="0.0.1",
+    author="Sam Reeder",
+    autor_email="s.reeder@ucl.ac.uk",
+    licence="MIT",
+    install_requires=['numpy',]
+)
