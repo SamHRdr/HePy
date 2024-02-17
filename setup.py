5557d43ea68c9aaefd2130cccad38b9ca20186e0
SamHRdr <45273109+SamHRdr@users.noreply.github.com>
2024-02-17 21:04:42 +0000

    Update setup.py
    Bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="HePy",
-    version="0.1.0",
+    version="0.1.1",
     author="Sam Reeder",
     author_email="s.reeder@ucl.ac.uk",
     licence="MIT",
