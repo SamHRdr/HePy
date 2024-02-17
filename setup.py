2df90b1c31bfe9bd1777d70f2f6f2fc06b1dc736
SamHRdr <45273109+SamHRdr@users.noreply.github.com>
2024-02-17 20:59:42 +0000

    Update setup.py
    Significant updates to Stark_Matrix.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="HePy",
-    version="0.0.5",
+    version="0.1.0",
     author="Sam Reeder",
     author_email="s.reeder@ucl.ac.uk",
     licence="MIT",
