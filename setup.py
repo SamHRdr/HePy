3861f6542168837bac166eef44d1aed019d42c84
SamHRdr <45273109+SamHRdr@users.noreply.github.com>
2024-02-26 21:07:51 +0000

    Update setup.py
    Added Inglis Teller limit function to HePy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="HePy",
-    version="0.1.3",
+    version="0.1.4",
     author="Sam Reeder",
     author_email="s.reeder@ucl.ac.uk",
     licence="MIT",
