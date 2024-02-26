c0ef4186ecdc4cab914675429640da81f1ed02b6
SamHRdr <45273109+SamHRdr@users.noreply.github.com>
2024-02-26 19:42:00 +0000

    Updated Stark_Matrix
    Changed the functions to include quantum defects in the dipole matrix elements as well as the field free energies.

--- HePy/Stark_Matrix.py
+++ HePy/Stark_Matrix.py
@@ -39,6 +39,35 @@ def Basis(ns,ml_arr):
                     basis.append(state)
     return basis
 
+def QD_Array(basis,dJ=1):
+    """Function to calculate the quantum defects of a Rydberg atom in a particular state.
+    
+    Inputs
+    -------
+    basis = 2D Array. Basis set of atomic states [n,l,ml].
+    dJ    = Difference from l i.e. J = l+dJ - used in quantum defect calculation.
+            (Default is 1. Can take values dJ = -1,0,+1 for the triplet state [S=1])
+    
+    Returns
+    --------
+    QD_arr = array of quantum defects for the input basis.
+     """
+    # Iniitlise square matrix the size of the basis
+    size = len(basis)
+    QD_arr = np.zeros(size)
+
+    for state in range(size):
+        # Extract quantum numbers
+        ni = basis[state][0]
+        li = basis[state][1]
+        Ji = li+dJ
+
+        # Calculate field free energies
+        D = defect(ni,li,Ji)
+        QD_arr[state] = D
+        
+    return QD_arr
+
 def order(eigenvalues,eigenvectors):
     """Sort eigenvalues and eigenvectors in ascending order of eigenvalue."""
     
@@ -68,12 +97,13 @@ def lookup_eigval(basis,n,l):
     
     return inds_nl
 
-def dip_mtrx_elem(basis1,basis2,q=0,r_exp=1,h=0.005,rcore=0.65):
+def dip_mtrx_elem(basis1,basis2,QD1,QD2,q=0,r_exp=1,h=0.005,rcore=0.65):
     """Calculate dipole matrix element <n'l'm'|r|nlm> per unit field.
     
     Inputs
     -------
     basis = 2D Array. Basis set of atomic states [n,l,ml].
+    QD    = Qauntum defect of basis state.
     q     = Electric field polarisation vector, q = 0 [linear polarised], q= +/- 1 [Circularly polarised]
     r_exp = Exponent of r in matrix element for radial integral. (Default = 1).
     step  = Radial integration step size. (Default = 0.005).
@@ -85,28 +115,29 @@ def dip_mtrx_elem(basis1,basis2,q=0,r_exp=1,h=0.005,rcore=0.65):
     """
     # state 1
     n1, l1, ml1 = basis1[0], basis1[1], basis1[2]
+    N1 = n1-QD1
 
     # state 2
     n2, l2, ml2 = basis2[0], basis2[1], basis2[2]
+    N2 = n2-QD2
 
     # Use numerov method to calculate radial integral
-    radint = radial_integral(n1,l1,n2,l2,r_exp,h,rcore)
-
+    radint = radial_integral(N1, l1, N2, l2, r_exp=1, h=0.005, rcore=0.65)
+    
     # Calculate angular initegral
-    angint = (-1)**ml2 * np.sqrt((2*l2+1)*(2*l1+1)) * float(wigner_3j(l2,1,l1,-ml2,q,ml1)) * float(wigner_3j(l2,1,l1,0,0,0))
+    angint = (-1.)**ml2 * np.sqrt((2*l2+1)*(2*l1+1)) * float(wigner_3j(l2,1,l1,-ml2,q,ml1)) * float(wigner_3j(l2,1,l1,0,0,0))
 
     dipole_matrix_element = angint*radint
 
     return dipole_matrix_element
 
-def H_0(basis,dJ=1):
+def H_0(basis,QD_arr):
     """Function to calculate the field free hamiltonian for a Rydberg atom.
     
     Inputs
     -------
-    basis = 2D Array. Basis set of atomic states [n,l].
-    dJ    = Difference from l i.e. J = l+dJ - used in quantum defect calculation.
-            (Default is 1. Can take values dJ = -1,0,+1 for the triplet state [S=1])
+    basis  = 2D Array. Basis set of atomic states [n,l,ml].
+    QD_arr = 1D array of quantum defects for each state in basis.
     
     Returns
     --------
@@ -120,11 +151,9 @@ def H_0(basis,dJ=1):
     for state in range(size):
         # Extract quantum numbers
         ni = basis[state][0]
-        li = basis[state][1]
-        Ji = li+dJ
 
         # Calculate field free energies
-        D = defect(ni,li,Ji)
+        D = QD_arr[state]
         W = W_tot(ni,D)
         En.append(W)
 
@@ -132,22 +161,23 @@ def H_0(basis,dJ=1):
     np.fill_diagonal(H0,En)
     return H0
 
-def H_s(basis,Fz=1,q=0,r_exp=1,step=0.005,rcore=0.65):
+def H_s(basis,QD_arr,Fz=1.0,q=0,r_exp=1,step=0.005,rcore=0.65):
     """Calculate the off-diagonal matrix elements that make up the field hamiltonian Hs
     
     Inputs
     -------
-    basis = 2D Array. Basis set of atomic states [n,l].
-    Fz    = Electric field magnitude (V/m) [Default = 1V/m]
-    q     = Electric field polarisation vector, q = 0 [linear polarised], q= +/- 1 [Circularly polarised]
+    basis  = 2D Array. Basis set of atomic states [n,l,ml].
+    QD_arr = 1D array of quantum defects for each state in basis.
+    Fz     = Electric field magnitude (V/m) [Default = 1V/m]
+    q      = Electric field polarisation vector, q = 0 [linear polarised], q= +/- 1 [Circularly polarised]
             (Default = 0, linearly polarised)
-    r_exp = Exponent of r in matrix element for radial integral. (Default = 1).
-    step  = Radial integration step size. (Default = 0.005).
-    rcore = Minimum r value in numerov. (The default is 0.65 -> dipoole polarisability of He core).
+    r_exp  = Exponent of r in matrix element for radial integral. (Default = 1).
+    step   = Radial integration step size. (Default = 0.005).
+    rcore  = Minimum r value in numerov. (The default is 0.65 -> dipoole polarisability of He core).
     
     Returns
     -------
-    Hs = Stark matrix in units of Hz.
+    Hs = Stark matrix in units of Hz [multiply by h for SI].
     """
     
     # Initialise matrix
@@ -159,11 +189,13 @@ def H_s(basis,Fz=1,q=0,r_exp=1,step=0.005,rcore=0.65):
         ni  = basis[i][0]
         li  = basis[i][1]
         mli = basis[i][2]
+        QDi = QD_arr[i]
         
         for j in range(size):
             nj  = basis[j][0]        
             lj  = basis[j][1]
             mlj = basis[j][2]
+            QDj = QD_arr[j]
             
             ## Conditions for calculation
             if i!=j: # off diagonal
@@ -175,7 +207,7 @@ def H_s(basis,Fz=1,q=0,r_exp=1,step=0.005,rcore=0.65):
                         state2 = [nj,lj,mlj]
 
                         ## Populate [i,j] element of matrix
-                        Hs[i,j] = dip_mtrx_elem(state1,state2,q,r_exp,step,rcore)
+                        Hs[i,j] = dip_mtrx_elem(state1,state2,QDi,QDj,q,r_exp,step,rcore)
                     
     return (Hs * Fz * e * a0 / h)
 
@@ -190,8 +222,8 @@ def E_Stark(H0,Hs,Fz_arr):
     
     Returns
     -------
-    Eigvals = Correspond to the stark energies.
-    Eigvecs = Correspond to the wavefunctions. 
+    Eigvals = Correspond to the stark energies - index as [state, field].
+    Eigvecs = Correspond to the wavefunctions - index as [field, basis, state]. 
     """
     size_H0 = np.shape(H0)[0]
     size_Hs = np.shape(Hs)[0]
