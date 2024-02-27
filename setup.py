91c2695ef4a9dde909394cb02d88af40194ba127
SamHRdr <45273109+SamHRdr@users.noreply.github.com>
2024-02-27 23:00:31 +0000

    Update setup.py
    Added singlet quantum defects

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="HePy",
-    version="0.1.5",
+    version="0.1.6",
     author="Sam Reeder",
     author_email="s.reeder@ucl.ac.uk",
     licence="MIT",
