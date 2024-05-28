8bcdf26b3f7d28ef43838ff7c9de382fc7818c38
SamHRdr <45273109+SamHRdr@users.noreply.github.com>
2024-05-28 17:57:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="HePy",
-    version="0.2.6",
+    version="0.2.7",
     author="Sam Reeder",
     author_email="s.reeder@ucl.ac.uk",
     licence="MIT",
